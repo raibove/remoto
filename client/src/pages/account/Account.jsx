434687a9b9b29443ef268ea3e8d53ff2ddde446d
Shweta Kale <shwetakale144@gmail.com>
2022-01-29 22:57:47 +0530

    added links

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -3,116 +3,37 @@ import SideBar from "../../components/sidebar/SideBar"
 import {Table, Tag, Upload, Tabs, message, notification,Button} from "antd";
 import {Link} from "react-router-dom"
 import { signup, getAllEmployee,addMultipleEmployee, getPendingEmployee } from "../../redux/actions/userAction";
-import { connect } from "react-redux";
-import xlsx from "xlsx"
+import { connect } from "react-redux"
 import store from "../../redux/store";
-import { UploadOutlined } from '@ant-design/icons';
+import xlsx from "xlsx"
+import "./Account.css"
 
-const {Column} = Table
-
-const { TabPane } = Tabs;
 const Account = (props)=>{
-    const [empData, setEmpData] = useState([]);
-    const [pendingEmpData, setPendingEmpData] = useState([])
-
-    
-    useEffect(()=>{
-        console.log("here")
-        if(props.pending_employee===null){
-            props.getPendingEmployee()
-        console.log("hereii")
-        }else{
-            console.log(props.pending_employee)
-            setPendingEmpData(props.pending_employee.documents)
-        }
-    },[props.pending_employee])
-
-    useEffect(()=>{
-        if(props.all_employee===null){
-            props.getAllEmployee()
-        }else{
-            console.log(props.all_employee)
-            setEmpData(props.all_employee.documents)
-        }
-    },[props.all_employee])
-
-   
-    const close = () => {
-        store.dispatch({ type: "SET_ALERT", payload: { message: null } });
-      };
-    
-      
-    const openNotification = (err) => {
-      notification["error"]({
-        message: "Error in signup",
-        description: err.message,
-        onClose: close,
-      });
-    };
   
-    useEffect(() => {
-      if (props.alert_message !== null && props.alert_message!= undefined) {
-        openNotification(props.alert_message.data);
-       console.log(props.alert_message.data)
-      }
-    }, [props.alert_message]);
-  
-    
-    const readUploadFile =  (e) => {
-        e.preventDefault();
-        console.log("called")
-       
-        console.log(e.target.files)
-        if (e.target.files) {
-            const reader = new FileReader();
-            reader.onload = async (e) => {
-                const data = e.target.result;
-                const workbook = xlsx.read(data, { type: "array" });
-                const sheetName = workbook.SheetNames[0];
-                const worksheet = workbook.Sheets[sheetName];
-                const json = xlsx.utils.sheet_to_json(worksheet);
-                console.log(json);
-                //return json;
-                try{
-                let eres = await props.addMultipleEmployee(json)
-                }catch(err){
-                    console.log(err)
-                }
-            };
-            reader.readAsArrayBuffer(e.target.files[0]);
-            e.target.value = ''
-        }
-        
-        return null;
-    }
-
-
     return(
         <>
         <SideBar/>
-        <div className="all-employee-container">
-            <div style={{height:"50px"}}>
-            <Link to="/new-employee">
-            <Button type="primary" className="employee-add">
-                Add Employee
+        <div className="account-microsoft-container">
+            <Link to="https://admin.microsoft.com/AdminPortal/Home?#/users/:/adduser">
+            <Button type="primary" className="account-microsoft-add">
+                Add a User
             </Button>
             </Link>
-            <Link to="/new-employee">
-            <Button type="primary" className="employee-add">
-                Add Employee
+            <Link to="https://admin.microsoft.com/AdminPortal/Home?#/users/:/addmultipleusers">
+            <Button type="primary" className="account-microsoft-add">
+                Add Multiple User
             </Button>
             </Link>
-            <Link to="/new-employee">
-            <Button type="primary" className="employee-add">
-                Add Employee
+            <Link to="https://account.activedirectory.windowsazure.com/UserManagement/MultifactorVerification.aspx?BrandContextID=O365">
+            <Button type="primary" className="account-microsoft-add">
+                Add Multifactor Authentication
             </Button>
             </Link>
-            <Link to="/new-employee">
-            <Button type="primary" className="employee-add">
-                Add Employee
+            <Link to="https://admin.microsoft.com/AdminPortal/Home?#/users/:/DeleteUser">
+            <Button type="primary" className="account-microsoft-add">
+                Delete User
             </Button>
             </Link>
-            </div>
         </div>
         <div>
         </div>
